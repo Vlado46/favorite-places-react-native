c8cbe6f24be0a40c71fc5d63ea0ae994b34caea3
Vlado46 <110978223+Vlado46@users.noreply.github.com>
2024-01-21 13:28:55 +0100

    Initial

---
+++ components/Places/PlaceForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const PlaceForm = () => {
+  return (
+    <View>
+      <Text>place form</Text>
+    </View>
+  );
+};
+
+export default PlaceForm;
